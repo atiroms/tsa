fab3e375f56b25414a27648200c3273bd1c894c5
atiroms <atirom.umusus@gmail.com>
2019-11-07 20:23:27 +0900

    moved order history analysis to analyze.py

--- analyze.py
+++ analyze.py
@@ -1,32 +1,58 @@
 
 import os
 import numpy as np
+import matplotlib.pyplot as plt
 
 class Analyze:
     def __init__(self,path_src='C:/Users/atiro/Dropbox/tsa/20191107_161734'):
         self.path_src=path_src
 
     def order2carpet(self,range_rate=1000,int_rate=10):
+        list_file=os.listdir(self.path_src)
+        list_time_file=list(set([f[:15] for f in list_file]))
+        list_time_file.sort()
+        for time_file in list_time_file:
+            array_rate=np.load(os.path.join(self.path_src,time_file+'_rate.npy'))
+            array_asks=np.load(os.path.join(self.path_src,time_file+'_asks.npy'))
+            array_bids=np.load(os.path.join(self.path_src,time_file+'_bids.npy'))
 
-        
-        array_order=np.ndarray([4,2*range_rate])
-        array_order[0,:]=np.arange(-range_rate*int_rate,range_rate*int_rate,int_rate)
-        for i in range(range_rate):
-            if df_bids.loc[0,'rate']>rate-(range_rate-i)*int_rate:
-                id_max=max(df_bids.index[df_bids['rate']>rate-(range_rate-i)*int_rate])
-                array_order[1,i]=-df_bids.loc[id_max,'accumulate']
-            else:
-                array_order[1,i]=0
+            n_time=np.shape(array_rate)[0]
+            array_order=np.ndarray([n_time,2*range_rate])
 
-            if df_asks.loc[0,'rate']<rate+(range_rate-i-1)*int_rate:
-                id_max=max(df_asks.index[df_asks['rate']<rate+(range_rate-i-1)*int_rate])
-                array_order[1,2*range_rate-i-1]=df_asks.loc[id_max,'accumulate']
-            else:
-                array_order[1,2*range_rate-i-1]=0
+            for idx_time in range(n_time):
+                rate=array_rate[idx_time,1]
+                accum_amount=0.0
+                current_step_rate=0
+                for idx_order in range(np.shape(array_asks)[1]):
+                    diff_rate=array_asks[idx_time,idx_order,0]-rate
+                    reached_step_rate=int(diff_rate/int_rate)
+                    if reached_step_rate>range_rate:
+                        reached_step_rate=range_rate
+                        array_order[idx_time,(range_rate+current_step_rate):(range_rate+reached_step_rate)]=accum_amount
+                        #break
+                    else:
+                        array_order[idx_time,(range_rate+current_step_rate):(range_rate+reached_step_rate)]=accum_amount
+                        current_step_rate=reached_step_rate
+                        accum_amount=accum_amount+array_asks[idx_time,idx_order,1]
 
-        fit_order=np.polyfit(array_order[0],array_order[1],1)
-        array_order[2,:]=array_order[1,:]-array_order[0,:]*fit_order[0]
-        sigma=np.std(array_order[2,:])
-        array_order[3,:]=array_order[2,:]/sigma
+                accum_amount=0.0
+                current_step_rate=0
+                for idx_order in range(np.shape(array_bids)[1]):
+                    diff_rate=rate-array_bids[idx_time,idx_order,0]
+                    reached_step_rate=int(diff_rate/int_rate)
+                    if reached_step_rate>range_rate:
+                        reached_step_rate=range_rate
+                        array_order[idx_time,(range_rate-reached_step_rate):(range_rate-current_step_rate)]=-accum_amount
+                        #break
+                    else:
+                        array_order[idx_time,(range_rate-reached_step_rate):(range_rate-current_step_rate)]=-accum_amount
+                        current_step_rate=reached_step_rate
+                        accum_amount=accum_amount+array_bids[idx_time,idx_order,1]
+                        os.path.join(self.path_src, time_file+'_order.npy')
 
-        array_order_history=np.append(array_order_history,np.reshape(array_order[3,:],(1,range_rate*2)),axis=0)
+            np.save(os.path.join(self.path_src,time_file+'_order.npy'),array_order)
+
+        fig, ax = plt.subplots()
+        heatmap = ax.pcolor(array_order)
+
+        plt.show()
--- test.py
+++ test.py
@@ -13,126 +13,13 @@ import matplotlib.pyplot as plt
 from publicAPI import CCPublic as CCPublic
 from privateAPI import CCPrivate as CCPrivate
 from acquire import Acquire as Acquire
+from analyze import Analyze as Analyze
+
+analyze=Analyze()
+analyze.order2carpet()
 
 acquire=Acquire()
 acquire.record_order()
-
-range_rate=1000
-int_rate=10
-
-path_src='C:/Users/atiro/Dropbox/tsa/20191107_161734'
-list_file=os.listdir(path_src)
-list_time_file=list(set([f[:15] for f in list_file]))
-list_time_file.sort()
-for time_file in list_time_file:
-	array_rate=np.load(os.path.join(path_src,time_file+'_rate.npy'))
-	array_asks=np.load(os.path.join(path_src,time_file+'_asks.npy'))
-	array_bids=np.load(os.path.join(path_src,time_file+'_bids.npy'))
-
-	n_time=np.shape(array_rate)[0]
-	array_order=np.ndarray([n_time,3,2*range_rate])
-
-	for idx_time in range(n_time):
-		rate=array_rate[idx_time,1]
-		accum_amount=0.0
-		current_step_rate=0
-		for idx_order in range(np.shape(array_asks)[1]):
-			diff_rate=array_asks[idx_time,idx_order,0]-rate
-			reached_step_rate=int(diff_rate/int_rate)
-			if reached_step_rate>range_rate:
-				reached_step_rate=range_rate
-				array_order[idx_time,0,(range_rate+current_step_rate):(range_rate+reached_step_rate)]=accum_amount
-				break
-			else:
-				array_order[idx_time,0,(range_rate+current_step_rate):(range_rate+reached_step_rate)]=accum_amount
-				current_step_rate=reached_step_rate
-				accum_amount=accum_amount+array_asks[idx_time,idx_order,1]
-				
-		accum_amount=0.0
-		current_step_rate=0
-		for idx_order in range(np.shape(array_bids)[1]):
-			diff_rate=rate-array_bids[idx_time,idx_order,0]
-			reached_step_rate=int(diff_rate/int_rate)
-			if reached_step_rate>range_rate:
-				reached_step_rate=range_rate
-				array_order[idx_time,0,(range_rate-reached_step_rate):(range_rate-current_step_rate)]=-accum_amount
-				break
-			else:
-				array_order[idx_time,0,(range_rate-reached_step_rate):(range_rate-current_step_rate)]=-accum_amount
-				current_step_rate=reached_step_rate
-				accum_amount=accum_amount+array_asks[idx_time,idx_order,1]
-
-fig, ax = plt.subplots()
-heatmap = ax.pcolor(array_order[:,0,:])
-#plt.plot(array_order[0],array_order[3])
-
-plt.show()
-
-
-ccpublic = CCPublic()
-
-# Request order with intervals
-array_order_history=np.ndarray([0,2*range_save])
-array_rate=np.ndarray([0,2])
-starttime=int(np.ceil(time.time()))
-while True:
-  time.sleep(interval_sampling - ((time.time() - starttime) % interval_sampling))
-  timestamp=int(time.time())
-  if time.time()-starttime>length_sampling:
-    break
-  print(str(timestamp-starttime))
-
-  rate=int(ccpublic.f_ticker()['last'])
-  array_rate=np.append(array_rate,np.reshape(np.array([timestamp,rate]),(1,2)),axis=0)
-  order=ccpublic.f_order()
-  df_asks=pd.DataFrame(columns=['rate','amount','accumulate'])
-  df_bids=pd.DataFrame(columns=['rate','amount','accumulate'])
-  for value in order['asks']:
-    if len(df_asks)>0:
-      add=[int(float(value[0])),float(value[1]),df_asks.loc[len(df_asks)-1,'accumulate']+float(value[1])]
-    else:
-      add=[int(float(value[0])),float(value[1]),+float(value[1])]
-    df_asks.loc[len(df_asks),:]=add
-
-  for value in order['bids']:
-    if len(df_bids)>0:
-      add=[int(float(value[0])),float(value[1]),df_bids.loc[len(df_bids)-1,'accumulate']+float(value[1])]
-    else:
-      add=[int(float(value[0])),float(value[1]),+float(value[1])]
-    df_bids.loc[len(df_bids),:]=add
-
-  array_order=np.ndarray([4,2*range_save])
-  array_order[0,:]=np.arange(-range_save*int_save,range_save*int_save,int_save)
-  for i in range(range_save):
-    if df_bids.loc[0,'rate']>rate-(range_save-i)*int_save:
-      id_max=max(df_bids.index[df_bids['rate']>rate-(range_save-i)*int_save])
-      array_order[1,i]=-df_bids.loc[id_max,'accumulate']
-    else:
-      array_order[1,i]=0
-
-    if df_asks.loc[0,'rate']<rate+(range_save-i-1)*int_save:
-      id_max=max(df_asks.index[df_asks['rate']<rate+(range_save-i-1)*int_save])
-      array_order[1,2*range_save-i-1]=df_asks.loc[id_max,'accumulate']
-    else:
-      array_order[1,2*range_save-i-1]=0
-
-  fit_order=np.polyfit(array_order[0],array_order[1],1)
-  array_order[2,:]=array_order[1,:]-array_order[0,:]*fit_order[0]
-  sigma=np.std(array_order[2,:])
-  array_order[3,:]=array_order[2,:]/sigma
-
-  array_order_history=np.append(array_order_history,np.reshape(array_order[3,:],(1,range_save*2)),axis=0)
-
-fig, ax = plt.subplots()
-heatmap = ax.pcolor(array_order_history)
-#plt.plot(array_order[0],array_order[3])
-
-plt.show()
-
-try:
-  requests.get('https://coincheck.com/api/ticke').json()
-except:
-  print("error")
 
 ## Private API
 
