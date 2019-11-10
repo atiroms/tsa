ee6fd08eb555ab63d9ffba199897b88334ccafce
atiroms <atirom.umusus@gmail.com>
2019-11-10 12:06:22 +0900

    minor change

--- tutorial/air.py
+++ tutorial/air.py
@@ -104,6 +104,7 @@ model = models.Sequential()
 model.add(layers.LSTM(50, input_shape=(train_X.shape[1], train_X.shape[2])))
 model.add(layers.Dense(1))
 model.compile(loss='mae', optimizer='adam')
+model.summary()
 # fit network
 history = model.fit(train_X, train_y, epochs=50, batch_size=72, validation_data=(test_X, test_y), verbose=2, shuffle=False)
 # plot history
